5a2636a1647ed501181048f1fd1cf96066bacae2
SoaresMariaEduarda <108130589+SoaresMariaEduarda@users.noreply.github.com>
2023-11-06 08:49:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-alert('Olá mundo');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom'). onclick = tocaSomPom;
+
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap'). onclick = tocaSomClap;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim'). onclick = tocaSomTim;
+
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff'). onclick = tocaSomPuff;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splah'). onclick = tocaSomSplash;
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim'). onclick = tocaSomToim;
